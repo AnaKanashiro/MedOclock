91fe7324b68525f2176e57b82710f8852316ddbc
Ana Kanashiro <bikoressawa@gmail.com>
2025-05-08 16:35:02 -0300

    header e logo feitas

---
+++ componentes/Logo.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import { View, Text, StyleSheet, Image } from 'react-native';
+import logo from '../assets/logo.png';
+import logo2 from '../assets/logo2.png';
+import logo3 from '../assets/logo3.png';
+
+function Logo() {
+    return (
+        <View style={styles.logoContainer}>
+            <Image source={logo3} style={styles.logoImage} />
+            <Text style={styles.logoText}>
+                <Text style={styles.boldText}>Med </Text>o'Clock
+            </Text>
+        </View>
+    );
+}
+
+const styles = StyleSheet.create({
+    logoContainer: {
+        flexDirection: 'row',
+        alignItems: 'center',
+        margin: 20,
+    },
+    logoText: {
+        fontSize: 35,
+        color: '#FF407D',
+    },
+    boldText: {
+        fontWeight: 'bold',
+    },
+    logoImage: {
+        marginRight: 10,
+        width: 60,
+        height: 60,
+      },
+});
+
+export default Logo;
